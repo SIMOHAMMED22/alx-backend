a6b80161ea2cdbcb67a372759ac26e1bb28e05c4
SIMOHAMMED22 <simofinance22@gmail.com>
2024-04-04 17:32:52 +0000

    basic

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,46 +1,27 @@
 #!/usr/bin/env python3
 """ BasicCache module """
-
-
-class BaseCaching:
-    def __init__(self):
-        """
-        Initialize the object with an empty dictionary to store cached data.
-        """
-        self.cache_data = {}
-
-    def print_cache(self):
-        print(self.cache_data)
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    def __init__(self):
-        """
-        Constructor for the class, initializes the object.
-        """
-        super().__init__()
+    """ BasicCache class:
+    - Inherits from BaseCaching.
+    - This caching system doesn’t have limit.
+    """
 
     def put(self, key, item):
         """
-        Puts the item into the cache_data dictionary with the given key.
-
-        :param key: The key for the item
-        :param item: The item to be cached
-        :return: None
+        Assign to the dictionary self.cache_data
+        the item value for the key key.
+        If key or item is None, this method should not do anything.
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
         """
-        - Description: Get the value from the cache_data
-        dictionary based on the input key.
-        - Parameters:
-            - key: The key to look up in the cache_data dictionary.
-        - Return:
-            - If the key is found in the cache_data, return the
-            corresponding value. Otherwise, return None.
+        Return the value in self.cache_data linked to key.
+        If key is None or if the key doesn’t exist in
+        self.cache_data, return None.
         """
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        return self.cache_data.get(key) if key is not None else None
